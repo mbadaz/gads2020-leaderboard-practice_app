594a7dd8434b523cfa8132ce133b30be834cfa98
mbadaz <tapsmuzira@gmail.com>
2020-09-11 12:58:47 +0200

    added dependencies

---
+++ app/src/main/java/com/mambure/myapplication/dependencyinjection/AppModule.java
@@ -0,0 +1,12 @@
+package com.mambure.myapplication.dependencyinjection;
+
+import dagger.Module;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ApplicationComponent;
+
+@Module
+@InstallIn(ApplicationComponent.class)
+public class AppModule {
+
+
+}
